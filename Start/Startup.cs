805a96fd709c7c87e07951d74b42cf6f8ec7eb4c
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2017-12-07 14:20:31 +0200

    Mym

--- Start/Startup.cs
+++ Start/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Start
 {
@@ -16,16 +17,37 @@ namespace Start
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.Use(next =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+                };
+            });
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/wp"
+            });
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
