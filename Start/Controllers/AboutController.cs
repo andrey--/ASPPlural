28bd0230428481f9a4b9f1df20550f1b346696ee
andrey-- <dreamcaster@ukr.net>
2017-12-10 21:31:51 +0200

    new COntrollers

--- Start/Controllers/AboutController.cs
+++ Start/Controllers/AboutController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Start.Controllers
 {
+    [Route("[controller]/[action]/")]
     public class AboutController
     {
         public string Phone()
