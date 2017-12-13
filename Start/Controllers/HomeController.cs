d985861bb7feb74168c8326a3362ee7bcb25d80b
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2017-12-13 22:02:10 +0200

    Introducing ViewModel

--- Start/Controllers/HomeController.cs
+++ Start/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Start.Models;
 using Start.Services;
+using Start.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,19 @@ namespace Start.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData )
+        public HomeController(IRestaurantData restaurantData,
+                               IGreeter greeter)
         {
             _restaurantData = restaurantData;
-            
+            _greeter = greeter;
         }
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
             return View(model);
         }
     }
